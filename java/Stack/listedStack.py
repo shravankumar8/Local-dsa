a594d417ee7027c72926f80022f4a0b7a66c373d
Venkey Shravan <provenworks@Venkeys-MacBook-Pro.local>
2025-01-11 17:11:18 +0530

    done

--- java/Stack/listedStack.py
+++
@@ -1,7 +0,0 @@
-stack=[]
-stack.append('s')
-stack.append('s')
-stack.append('s')
-stack.append('s')
-print('INitis stack')
-print(stack)
--- problems/reverseNmber.py
+++
@@ -1,3 +0,0 @@
-number=int(input())
-reversedNUmber=int(str(number)[::-1])
-print("Reversed Number:", reversedNUmber)
